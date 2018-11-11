bb6a544ec2b7360dfbd27e8de8bac0f9f90a27fa
ALI <alihusseinalissa@yahoo.com>
2018-11-11 23:54:19 +0300

    Code development

--- app/src/main/java/com/ece/tripplea/ChildActivity.java
+++ app/src/main/java/com/ece/tripplea/ChildActivity.java
@@ -14,6 +14,12 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
@@ -26,6 +32,11 @@ import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
 
 public class ChildActivity extends AppCompatActivity {
 
@@ -49,7 +60,7 @@ public class ChildActivity extends AppCompatActivity {
                     // ...
                     tv.setText(location.getLatitude() + ", " + location.getLongitude());
 //                    Toast.makeText(ChildActivity.this, "2", Toast.LENGTH_SHORT).show();
-
+                    pushToDatabase(location);
                 }
             }
         };
@@ -92,6 +103,58 @@ public class ChildActivity extends AppCompatActivity {
 
 
     }
+
+    private void pushToDatabase(Location location) {
+
+    }
+
+//    public class GsonRequest<T> extends Request<T> {
+//        private final Gson gson = new Gson();
+//        private final Class<T> clazz;
+//        private final Map<String, String> headers;
+//        private final Response.Listener<T> listener;
+//
+//        /**
+//         * Make a GET request and return a parsed object from JSON.
+//         *
+//         * @param url URL of the request to make
+//         * @param clazz Relevant class object, for Gson's reflection
+//         * @param headers Map of request headers
+//         */
+//        public GsonRequest(String url, Class<T> clazz, Map<String, String> headers,
+//                           Response.Listener<T> listener, Response.ErrorListener errorListener) {
+//            super(Method.POST, url, errorListener);
+//            this.clazz = clazz;
+//            this.headers = headers;
+//            this.listener = listener;
+//        }
+//
+//        @Override
+//        public Map<String, String> getHeaders() throws AuthFailureError {
+//            return headers != null ? headers : super.getHeaders();
+//        }
+//
+//        @Override
+//        protected void deliverResponse(T response) {
+//            listener.onResponse(response);
+//        }
+//
+//        @Override
+//        protected Response<T> parseNetworkResponse(NetworkResponse response) {
+//            try {
+//                String json = new String(
+//                        response.data,
+//                        HttpHeaderParser.parseCharset(response.headers));
+//                return Response.success(
+//                        gson.fromJson(json, clazz),
+//                        HttpHeaderParser.parseCacheHeaders(response));
+//            } catch (UnsupportedEncodingException e) {
+//                return Response.error(new ParseError(e));
+//            } catch (JsonSyntaxException e) {
+//                return Response.error(new ParseError(e));
+//            }
+//        }
+//    }
 
 
     protected void createLocationRequest() {
