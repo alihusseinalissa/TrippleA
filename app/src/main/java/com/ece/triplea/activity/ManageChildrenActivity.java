cdb3815affea1877fe112100f9b1ecf24c26e0cc
ALI <alihusseinalissa@yahoo.com>
2019-05-10 01:10:21 +0300

    Pick image from gallery for new children

--- app/src/main/java/com/ece/triplea/activity/ManageChildrenActivity.java
+++ app/src/main/java/com/ece/triplea/activity/ManageChildrenActivity.java
@@ -1,14 +1,28 @@
 package com.ece.triplea.activity;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetDialog;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -36,6 +50,8 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.ece.triplea.R;
 import com.ece.triplea.model.Child;
 
@@ -43,7 +59,11 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class ManageChildrenActivity extends AppCompatActivity implements Response.Listener<JSONArray>, Response.ErrorListener, View.OnClickListener, AdapterView.OnItemClickListener {
 
@@ -66,12 +86,18 @@ public class ManageChildrenActivity extends AppCompatActivity implements Respons
     private int PAGE_NO_CHILDREN = 2;
     private int PAGE_LIST = 3;
 
+    ImageView imgPicked;
+    String imagePath;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_children);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
 
         SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("GLOBAL", Context.MODE_PRIVATE);
         userId = sharedPreferences.getLong("user_id", -1);
@@ -119,6 +145,34 @@ public class ManageChildrenActivity extends AppCompatActivity implements Respons
                     }
                 });
 
+                imgPicked = dialogView.findViewById(R.id.imagePicked);
+                Button buPick = dialogView.findViewById(R.id.buPick);
+                Button buClear = dialogView.findViewById(R.id.buClear);
+                buPick.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                                MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                        startActivityForResult(pickPhoto , 1);//one can be replaced with any action code
+
+                    }
+                });
+                buClear.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        imgPicked.setImageURI(Uri.parse(""));
+                        imagePath = "";
+//        SharedPreferences.Editor editor = sharedPreferences.edit();
+//
+//        editor.putString("User" + i + "Image", imagePath);
+//
+//        editor.commit();
+
+
+                    }
+                });
+
+
                 builder.show();
 
 
@@ -126,6 +180,7 @@ public class ManageChildrenActivity extends AppCompatActivity implements Respons
         });
 
         viewFlipper = findViewById(R.id.manage_children_flipper);
+
         volleyQueue = Volley.newRequestQueue(this);
 
 
@@ -166,6 +221,30 @@ public class ManageChildrenActivity extends AppCompatActivity implements Respons
             }
         });
 
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        0);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
 
     }
 
@@ -306,7 +385,7 @@ public class ManageChildrenActivity extends AppCompatActivity implements Respons
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public View getView(final int position, View convertView, ViewGroup parent) {
             LayoutInflater inflater = getLayoutInflater();
             View view = inflater.inflate(R.layout.child_row, null);
 
@@ -344,5 +423,34 @@ public class ManageChildrenActivity extends AppCompatActivity implements Respons
             }
         }
     }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                Uri selectedImage = data.getData();
+                imagePath = selectedImage.toString();
+                Bitmap ThumbImage;
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                    ThumbImage = ThumbnailUtils.extractThumbnail(bitmap,
+                            300, 300);
+                    imgPicked.setImageBitmap(ThumbImage);
+                } catch (IOException e) {
+                    imgPicked.setImageURI(selectedImage);
+                    e.printStackTrace();
+                }
+
+
+            }
+        }
+
+
+    }
+
 
 }
